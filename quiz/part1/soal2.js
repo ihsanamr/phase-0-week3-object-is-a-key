253c2e23ebe406162c553f6600919382033db67b
Ihsan Amri <ihsanamrimuyassar@gmail.com>
2026-03-01 11:12:40 +0700

    Menyelesaikan soal 2 part 1

---
+++ quiz/part1/soal2.js
@@ -0,0 +1,15 @@
+//cari faktor persekutuan terbesar
+function fpb(angka1, angka2) {
+  if (angka2 === 0) {
+    return Math.abs(angka1);
+  }
+
+  return fpb(angka2, angka1 % angka2);
+}
+
+// TEST CASES
+console.log(fpb(12, 16)); // 4
+console.log(fpb(50, 40)); // 10
+console.log(fpb(22, 99)); // 11
+console.log(fpb(24, 36)); // 12
+console.log(fpb(17, 23)); // 1
